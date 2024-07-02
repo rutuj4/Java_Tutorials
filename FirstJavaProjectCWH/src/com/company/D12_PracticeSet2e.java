7f991566c872931974ece1f052df38020b873a35
Rutuj Sudhindrakumar Shah <93345414+rutuj4@users.noreply.github.com>
2024-07-03 00:21:12 +0530

    Add files via upload

---
+++ FirstJavaProjectCWH/src/com/company/D12_PracticeSet2e.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class D12_PracticeSet2e {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 7;
+		int a = 7 * 49 / 7 + 35 / 7;
+		System.out.println(a);
+	}
+
+}
