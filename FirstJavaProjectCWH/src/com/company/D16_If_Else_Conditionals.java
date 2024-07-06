19835ee341ba4a950c6a437cc4868935e5192e60
Rutuj Sudhindrakumar Shah <93345414+rutuj4@users.noreply.github.com>
2024-07-07 00:55:22 +0530

    Add files via upload

---
+++ FirstJavaProjectCWH/src/com/company/D16_If_Else_Conditionals.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class D16_If_Else_Conditionals {
+
+	public static void main(String[] args) {
+// If - Else Statements				
+//		int age = 6;
+		int age = 19;
+		if (age > 18) {
+			System.out.println("Yes boy you can drive!");
+		}
+		else {
+			System.out.println("No boy you cannot drive yet!");
+		}
+	}
+
+}
