650d956acba8d9025d0eb873079df70e098fee18
Rutuj Sudhindrakumar Shah <93345414+rutuj4@users.noreply.github.com>
2024-07-12 12:59:41 +0530

    Add files via upload

---
+++ FirstJavaProjectCWH/src/com/company/D21_WhileLoop.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class D21_WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("While Loop");
+		int i=1;
+		while(i<=3) {
+			System.out.println(i);
+			i++;
+		}
+//		int j =100;
+//		while(j<=200) {
+//			System.out.println(j);
+//			j++;
+//		}
+		
+//		while(true) {
+//			System.out.println("I am an infinite while loop!");
+//		}
+	}
+}
