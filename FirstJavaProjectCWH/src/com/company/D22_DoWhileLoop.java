af74dfbb53c66f68da3491bc8cafd68ea1d87125
Rutuj Sudhindrakumar Shah <93345414+rutuj4@users.noreply.github.com>
2024-07-25 15:01:57 +0530

    Add files via upload

---
+++ FirstJavaProjectCWH/src/com/company/D22_DoWhileLoop.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class D22_DoWhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Do - While Loop");
+//		int a = 0;
+//		while(a<5) {
+//			System.out.println(a);
+//			a++;
+//		}
+		
+		
+//		int b = 0;
+//		do {
+//			System.out.println(b);
+//			b++;
+//		}while(b<5);
+	
+// Program to print first n natural numbers using do-while loop.		
+	System.out.println("Program to print first n natural numbers");
+	int c = 1;
+	
+	do {
+		System.out.println(c);
+		c++;
+	}while(c<=50);
+	}	
+}
