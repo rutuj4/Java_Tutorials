953d6225ec4eb1a7dc0efced37c89fc777f44329
Rutuj Sudhindrakumar Shah <93345414+rutuj4@users.noreply.github.com>
2024-08-29 18:40:35 +0530

    Add files via upload

---
+++ FirstJavaProjectCWH/src/com/company/D33_VarArgs.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class D33_VarArgs {
+//	static int sum(int a, int b) {
+//		return a + b;
+//	}
+//	
+//	static int sum(int a, int b, int c) {
+//		return a + b + c;
+//	}
+//	
+//	static int sum(int a, int b, int c, int d) {
+//		return a + b + c + d;
+//	}
+	
+	static int sum(int ...arr) {
+		// Available as int [] arr;
+		
+		int result = 0;
+		for(int a:arr) {
+			result +=a;
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Variable Arguments (varargs) tutorial");
+		System.out.println("The sum of 4 and 5 is: " + sum(4,5));
+		System.out.println("The sum of 4, 3 and 5 is: " + sum(4,3,5));
+		System.out.println("The sum of 2, 4, 3 and 5 is: " + sum(2,4,3,5));
+		System.out.println("The sum of 2, 4, 3 and 5 is: " + sum(1,2,4,3,5));
+		
+	}
+}
