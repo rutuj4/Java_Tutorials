436806b1e8a7c53243d7f8a6e60b78c0e80653fa
Rutuj Sudhindrakumar Shah <93345414+rutuj4@users.noreply.github.com>
2024-09-23 09:01:49 +0530

    Add files via upload

--- FirstJavaProjectCWH/src/com/company/D42_Constructors.java
+++ FirstJavaProjectCWH/src/com/company/D42_Constructors.java
@@ -1,9 +1,33 @@
 package com.company;
 
+class MyMainEmployee{
+	private int id;
+	private String name;
+	
+//	public MyMainEmployee() {
+//		id = 45;
+//		name = "Your-Name-Here";
+//	}
+	
+	public MyMainEmployee(String myName, int myId) {
+		id = myId;
+		name = myName;
+	}
+	
+	
+	public void setName(String n) { name = n; }
+	public String getName() { return name; }
+	public void setId(int i) { id = i;	}
+	public int getId() { return id;	}
+	
+}
+
 public class D42_Constructors {
 
 	public static void main(String[] args) {
-		
+		  MyMainEmployee harry = new MyMainEmployee("ProgrammingWithHarry",12);
+		  System.out.println(harry.getId());
+		  System.out.println(harry.getName());
 	}
 
 }
