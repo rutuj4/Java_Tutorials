4a9fa34f098c18f2252ec4b1aafc26ff401607d4
Rutuj Sudhindrakumar Shah <93345414+rutuj4@users.noreply.github.com>
2024-09-26 22:28:53 +0530

    Add files via upload

---
+++ FirstJavaProjectCWH/src/com/company/D43_Exercise3.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class D43_Exercise3 {
+
+	public static void main(String[] args) {
+		/*
+		 Create a class Game, which allows a user to play "Guess the number" game once
+		 Game should have the following methods:
+		 1. Constructor to generate the random number
+		 2. takeUserInput() to take a user input of number
+		 3. isCorrectNumber() to detect whether the number entered by the user is true
+		 4. getters and setters for number of guesses
+        */
+		
+	}
+
+}
