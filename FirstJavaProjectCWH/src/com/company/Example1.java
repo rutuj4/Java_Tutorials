c994bd97aac92c32292f02c0d8acd85d1a257e7a
Rutuj Sudhindrakumar Shah <93345414+rutuj4@users.noreply.github.com>
2024-10-02 22:37:54 +0530

    Add files via upload

--- FirstJavaProjectCWH/src/com/company/Example1.java
+++ FirstJavaProjectCWH/src/com/company/Example1.java
@@ -1,45 +1,56 @@
 package com.company;
 
+/*
 class Employeee{
-	int id;
-	String name;
 	int salary;
+	String name;
 	
 	public void printDetails() {
-		System.out.println("My id is "+id);
 		System.out.println("My name is "+name);
 		System.out.println("Salary is "+ salary);
 	}
 	
-	
 	public int getSalary() {
 		return salary;
 	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String n) {
+		name = n;
+	}
 }
+*/
 
+class CellPhone{
+	public void ring() {
+		System.out.println("Ringing...");
+	}
+	public void vibrate() {
+		System.out.println("Vibrating...");
+	}
+	public void callFriend() {
+		System.out.println("Calling Ciivii...");
+	}
+}
 public class Example1 {
 
 	public static void main(String[] args) {
-		System.out.println("This is our custom class");
+		/*
+		//Problem 1
 		Employeee harry = new Employeee();
-		Employeee john = new Employeee();
-		//Setting attributes
-		harry.id = 12;
-		harry.name="CodeWithHarry";
-		harry.salary=25000;
 		
-		john.id = 13;
-		john.name="John Khandelwal";
-		john.salary=35000;
+		harry.setName("CodeWithHarry");
+		System.out.println(harry.getName());
 		
-		//Printing the attributes
-		//System.out.println(harry.id);
-		//System.out.println(harry.name);
-		harry.printDetails();
-		john.printDetails();
-		int salary= john.getSalary();
-		System.out.println(salary);
+		harry.salary=233;
+		System.out.println(harry.getSalary());
+		*/
 	
+		CellPhone vivov3= new CellPhone();
+		
 	}
 
 } 
